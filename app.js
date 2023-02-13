c527d02fe5f10cf238b4634dd521b2d0ea2ed34b
sidiraos <raossidi@gmail.com>
2023-02-13 21:45:37 +0300

    bug fixed flipCard

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 let fruits = ['apple','banana','brocoli','cherry','pepper','straw'];
 
-const flipCard = document.querySelector('.flip-card');
+const flipCards = document.querySelectorAll('.flip-card');
+console.log(flipCards);
+// flipCards.addEventListener('click', flipCardHandler)
 
-flipCard.addEventListener('click', flipCardHandler)
+flipCards.forEach (flipCard => {
+    flipCard.addEventListener('click', flipCardHandler)
+})
 
-let flip = false;
+
 function flipCardHandler(e){
+    let flip = false;
+    let innerCard = e.target.closest('.flip-card-inner');
     if(flip){
-        document.querySelector('.flip-card-inner').style.transform = 'rotateY(0deg)';
+        innerCard.style.transform = 'rotateY(0deg)';
         flip = false;
     }else{
         flip = true;
         let randomIndex = Math.floor(Math.random() * fruits.length);
         let randomFruit = fruits[randomIndex];
-        document.querySelector('.fruit').setAttribute('src', `ressources/${randomFruit}.svg`);
-        document.querySelector('.flip-card-inner').style.transform = 'rotateY(180deg)';
+        e.target.setAttribute('src', `ressources/${randomFruit}.svg`);
+        innerCard.style.transform = 'rotateY(180deg)';
     }
-    
 }
